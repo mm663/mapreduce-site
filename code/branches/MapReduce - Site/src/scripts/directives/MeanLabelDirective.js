6d2c43cb79f3c903eff689f83e1099165c261ed4
mm663 <mm663@c3d71274-a883-4d27-a98c-5beadf4220e6>
2016-04-24 13:43:21 +0000

    Added support for removing already added pairs in Mean animation implementation.

---
+++ code/branches/MapReduce - Site/src/scripts/directives/MeanLabelDirective.js
@@ -0,0 +1,26 @@
+/**
+ * Created by matthewmicallef on 24/04/2016.
+ */
+
+var meanLabelDirective = function() {
+    return {
+        restrict: 'E',
+        scope: {
+            key: "@",
+            integer: "@"
+        },
+        replace: true,
+        template: "<span class='label label-default' ng-click='highlight()'>" +
+                    "Key: {{key}} Integer: {{integer}}" +
+                  "</span>",
+        link: function(scope, element, attrs) {
+            scope.highlight = function() {
+                if(element.hasClass('label-info')) {
+                    element.removeClass('label-info');
+                } else {
+                    element.addClass('label-info');
+                }
+            };
+        }
+    };
+};
